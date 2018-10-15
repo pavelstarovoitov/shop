c514f232846753b66863c6b5aff5d3730c23b22b
pavelstarovoitov <44179736+pavelstarovoitov@users.noreply.github.com>
2018-10-16 00:27:44 +0300

    Initial commit

---
+++ test_project/settings_prod.py
@@ -0,0 +1,17 @@
+DEBUG = False
+ALLOWED_HOSTS = ['*']
+
+
+
+DATABASES = {
+    'default':{
+        'ENGINE':'django.db.backends.postgresql_psycopg2',
+        'NAME':'db1',
+        'USER':'django_shop',
+        'PASSWORD':'',
+        'HOST':'localhost',
+        'PORT':'',
+
+
+    }
+}
